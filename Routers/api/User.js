6e1ef28224f753cc97aee3e83827510ba79039e4
robert thornton <robert.thornton92@gmail.com>
2025-09-25 12:45:46 +0300

    Feature - added click feature to navigate into nested folders. Displays nested folders and files inside the drive view and passes in the necesary files, folders, and user to render the ejs view

--- Routers/api/User.js
+++ Routers/api/User.js
@@ -5,21 +5,28 @@ const multer  = require('multer')
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage })
 
-// I need to rethink the structure of the application, the necessary routers 
-// and their roles in the application
+// User router is used to handle user-specific views and actions
+// such as viewing their drive and logging out.
+// Also, it handles any overlapping routes that include both files and folders.
 
 router.get('/drive', async (req, res) => {
     const folders = await prisma.folder.findMany({ where: { userId: req.user.id } });
     const files = await prisma.file.findMany({ where: { userId: req.user.id } });
 
-    res.render('drive', { user: req.user, folders, files });
+    res.render('drive', { user: req.user, folders, files, folderName: 'Root' });
     });
 
-    // should I move this to a different router?
-    router.post('/upload', upload.single('file'), async (req, res) => {
-
-    res.json({ file: req.file, user: req.user });
-})
+// Retrieve and display selected folder's contents in the drive view
+router.get('/drive/:folderId', async (req, res) => {
+  const folderId = req.params.folderId;
+  const folderName = await prisma.folder.findUnique({
+    where: { id: folderId },
+    select: { name: true }
+  });
+  const folders = await prisma.folder.findMany({ where: { parentId: folderId } });
+  const files = await prisma.file.findMany({ where: { userId: req.user.id, folderId: folderId } });
+  res.render('drive', { user: req.user, folders, files, folderName: folderName.name || 'Root' });
+});
 
 router.get('/logout', (req, res) => {
     req.logout(err => {
