2d9a66f0082c04f7c218abcb0b6dafaacb4076f1
robert thornton <robert.thornton92@gmail.com>
2026-04-22 10:31:48 +0300

    MIGRATION TO PRISMA V7 FROM V6

--- db/prisma.js
+++ db/prisma.js
@@ -1,6 +1,14 @@
 // prisma client to be used among all required instances of prisma clients
-// - removes multiple instances of prisma client that owuld slow down application
-const { PrismaClient } = require('../generated/prisma');
-const prisma = new PrismaClient();
+// - removes multiple instances of prisma client that would slow down application
+import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-module.exports = prisma;
+// Ensure DATABASE_URL is typed as a string
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+// Export a typed Prisma client instance
+export const prisma = new PrismaClient({ adapter });
